b6dfd49ce109a86a9c4ce6ce0ecb8daf03a39390
victornani <victor.nani.cs@gmail.com>
2022-12-02 13:41:46 +0200

    Added Buyer class and adjusted Product class

--- sweets-shop.cpp
+++ sweets-shop.cpp
@@ -10,16 +10,16 @@ class Product {
         char *name = NULL;
         double price;
         int promotions[7];
+        static int instances;
     
     public:
-        Product(int id) : id(id) { }
+        Product() :id(Product::instances++) { }
 
         Product(
-            int id,
             const char *name,
             double price,
             int promotions[]
-        ) :id(id) {
+        ) :id(Product::instances++) {
             this->name = new char[strlen(name) + 1];
             if(this->name == NULL) {
                 cout << "Memory allocation error on Product() constructor, this->name field." << endl;
@@ -38,7 +38,7 @@ class Product {
             }
         }
 
-        Product(const Product& product) :id(product.id) {
+        Product(const Product& product) :id(Product::instances++) {
             this->setPrice(product.price);
             this->setName(product.name);
             this->setPromotions(product.promotions, 7);
@@ -116,6 +116,64 @@ class Product {
             return *this;
         }
 
+        int &operator[](int index) {
+            if(index < 0 || index >= 7){
+                cout << endl << "Invalid  day (Must be between 0 and 7).";
+                exit(1);
+            }
+            return this->promotions[index];
+        }
+
+        Product& operator--(){
+            this->price--;
+            return *this;
+        }
+        
+        Product operator--(int){
+            Product prev = *this;
+            --(*this);
+            return prev;
+        }
+
+        operator double(){
+            return price;
+        }
+
+        bool operator!(){
+            for(int i = 0; i < 7; ++i) {
+               if(promotions[i] != 0) {
+                   return true;
+               }
+            }
+
+            return false;
+        }
+
+        bool operator>(const Product& product){
+            return this->price > product.price;
+        }
+
+        bool operator ==(const Product& product){
+            int counter1 = 0;
+            int counter2 = 0;
+            for(int i = 0; i < 7; ++i){
+                if(promotions[i] != 0)
+                    counter1++;
+            }
+            for(int i = 0; i < 7; ++i){
+                if(product.promotions[i]!= 0)
+                    counter2++;
+            }
+
+            return counter1 == counter2;
+        }
+
+        double calculateDiscount(int day) {
+            int promotion = promotions[day];
+            double discount = ((double)promotion / 100.0) * price;
+            return price - discount;
+        }
+
         friend ostream &operator<<(ostream& out, const Product& product) {
             if(product.name != NULL) {
                 out << "The " << product.name << " product with ID=" << product.id << " has a price of " << product.price << "RON. The promotions for this product per day are: " << endl;
@@ -133,35 +191,209 @@ class Product {
             return out;
         }
 
+        friend istream &operator>>(istream &in, Product& product) {
+            delete[] product.name;
+            char aux[100];
+            cout << "Introduce the product name: ";
+            in>>aux;
+            product.name = new char[strlen(aux) + 1];
+            strcpy(product.name, aux);
+
+            cout << "Introduce the price for this product: ";
+            in>>product.price;
+
+            cout << "Introduce the promotions for this product per day."<< endl;
+            cout << "   MONDAY:";
+            in>>product.promotions[0];
+            cout << "  TUESDAY:";
+            in>>product.promotions[1];
+            cout << "WEDNESDAY:";
+            in>>product.promotions[2];
+            cout << " THURSDAY:";
+            in>>product.promotions[3];
+            cout << "   FRIDAY:";
+            in>>product.promotions[4];
+            cout << " SATURDAY:";
+            in>>product.promotions[5];
+            cout << "   SUNDAY:";
+            in>>product.promotions[6];
+            cout << endl;
+
+            return in;
+        }
+
         ~Product() {
             delete[] name;
-            cout << "A product has been destroyed." << endl << endl;
+            --Product::instances;
+          //  cout << "A product has been destroyed." << endl;
         }
 };
 
+class Buyer {
+    private:
+        char name[30];
+        double budget;
+        int numberOfProducts;
+        int maxNumberOfProducts;
+        // HAS-A
+        Product *products;
+
+    public:
+        Buyer(
+            const char name[],
+            double budget
+        ) {
+            strcpy(this->name, name);
+            this->budget = budget;
+            numberOfProducts = 0;
+            maxNumberOfProducts = 1;
+            products = new Product[maxNumberOfProducts];
+            if(products == NULL) {
+                cout << "Memory allocation error. products was not properly initialized.";
+                exit(1);
+            }
+        }
+
+        double calculateTotalPrice() {
+            double sum = 0;
+            for(int i = 0; i < numberOfProducts; ++i) {
+                sum += (double)products[i];
+            }
+
+            return sum;
+        }
+
+        void addProduct(Product product, int day) {
+            if(products == NULL) {
+                products = new Product[maxNumberOfProducts];
+                if(products == NULL) {
+                    cout << "Memory allocation error. products was not properly initialized.";
+                    exit(1);
+                }
+            }
+
+            double totalPrice = calculateTotalPrice();
+            double newProductPrice = product.calculateDiscount(day);
+            if(totalPrice + newProductPrice > budget) {
+                cout << "The product " << product.getName() << " is too expensive for your budget." << endl;
+                return;
+            }
+
+            if(numberOfProducts == maxNumberOfProducts) {
+                Product *temp = products;
+                maxNumberOfProducts += 10;
+                products = new Product[maxNumberOfProducts];
+                for(int i = 0; i < numberOfProducts; ++i) {
+                    products[i] = temp[i];
+                }
+                
+                delete[] temp;
+            }
+            
+            product.setPrice(newProductPrice);
+            products[numberOfProducts] = product;
+            ++numberOfProducts;
+        }
+
+        void pay() {
+            double totalPrice = calculateTotalPrice();
+            budget -= totalPrice;
+            numberOfProducts = 0;
+            delete[] products;
+            products = NULL;
+            cout << "The shopping took " << totalPrice << "RON. The buyer " << name << " has a budget of " 
+                << budget << "RON left." << endl;
+        }
+
+        bool operator!() {
+            numberOfProducts = 0;
+            delete[] products;
+            products = NULL;
+
+            if(products != NULL) {
+                cout << "The products could not be deleted." << endl;
+                return 0;
+            }
+
+            cout << "Deleted all the products successfully." << endl;
+            return 1;
+        }
+
+        friend ostream &operator<<(ostream& out, const Buyer& buyer) {
+            out << "Buyer's name: " << buyer.name << endl;
+            out << "Buyer's budget: " << buyer.budget << endl;
+            out << "Number of products: " << buyer.numberOfProducts << endl;
+            if(buyer.numberOfProducts != 0) {
+                out << "The products are: " << endl;
+            }
+
+            for(int i = 0; i < buyer.numberOfProducts; ++i) {
+                cout << "Product " << i + 1 << ": " << buyer.products[i].getName() << " with price " 
+                    << (double)buyer.products[i] << "." << endl;
+            }
+
+            return out;
+        }
+
+        ~Buyer() {
+            delete[] products;
+           // cout << "A buyer has been destroyed." << endl;
+        }
+};
+
+int Product::instances = 0;
 
 int main(void) {
     int promotionsProduct1[] = {0, 10, 5, 10, 0, 0, 0};
-    Product product1(1, "Milka Oreo", 4.99, promotionsProduct1);
-    cout << product1 << endl;
+    Product product1("Milka Oreo", 4.99, promotionsProduct1);
+    // cout << product1 << endl;
 
-    int promotionsProduct2[] = {0, 5, 0, 12, 0, 0, 0};
-    Product product2(2);
+    int promotionsProduct2[] = {0, 10, 20, 0, 8, 0, 40};
+    Product product2;
     product2.setPrice(9.99);
     product2.setName("Nutella");
     product2.setPromotions(promotionsProduct2, 7);
-    cout << product2 << endl;
+    // cout << product2 << endl;
 
     Product product3(product2);
     product3 = product3 + 5.00;
-    cout << product3 << endl;
+    // cout << product3 << endl;
 
-    Product product4(4);
-    product4 = product1;
-    product4 += 5.00;
-    cout << product4 << endl;
+    // Product product4;
+    // product4 = product1;
+    // product4 += 5.00;
+    // cout << product4 << endl;
 
+    // Product product5;
+    // // cin >> product5;
+    // // cout << product5 << endl;
+    
+    // cout << "The discount for the first product on WEDNESDAY is: " << product1[2] << "%" << endl;
+    
+    // cout << endl << "The new price for the third product: \n";
+    // product4 = product3--;
+    // cout << product3 << endl;
+    // cout << product4 << endl;
+    
+    // cout << endl << "The new prices for the third and fourth product: \n";
+    // product4 = --product3;
+    // cout << product3 << endl;
+    // cout << product4 << endl;
+    
+    // cout << endl << "The price for the first product: " << double(product1) << endl;
 
+    // if(!product2)
+    //   cout << endl << "There are disscounts for the second product" << endl;
+    // else cout << endl << "There are NO disscounts for the second product" << endl;
+
+    // if(product1 > product2)
+    //     cout << endl << "The price of the first product is bigger than the price of the second product" << endl;
+    // else cout << endl << "The price of the first product is NOT bigger than the price of the second product" << endl;
+    
+    
+    // if(product1 == product2)
+    //     cout << endl << "The first product has the same number of days of disscounts as the second product" << endl;
+    // else cout << endl << "The first product has different number of days of disscounts " << endl;
     // int *promo = product1.getPromotions();
     // for(int i = 0; i < 7; ++i) {
     //     cout << promo[i] << " ";
@@ -169,5 +401,19 @@ int main(void) {
 
     // int promotionsProduct2[] = {0, -10, 5, 10, 0, 0, 0};
     // product1.setPromotions(promotionsProduct2, 7);
-    
+    int day = 1;
+    Buyer buyer("Madalina Demian", 200.23);
+    buyer.addProduct(product1, day);
+    buyer.addProduct(product2, day);
+    buyer.addProduct(product3, day);
+    cout << buyer << endl;
+    //cout << !buyer << endl;
+    cout << buyer << endl;
+    buyer.pay();
+    cout << buyer << endl;
+    buyer.addProduct(product1, day);
+    buyer.addProduct(product2, day);
+    cout << buyer << endl;
+    buyer.pay();
+
 }
